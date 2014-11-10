522777fd33a526f6b542e733be37a7302e77c4e2
hinadong <donghanbo@gmail.com>
2014-11-10 14:53:39 +0800

    change change
    change change

--- javascripts/tc.js
+++ javascripts/tc.js
@@ -9,7 +9,25 @@ jQuery(function() {
 		jQuery('#dropcon').hide();
 
 	})
-	jQuery(".crb a").hover(function() {
+
+})
+
+jQuery(".fullSlide").slide({
+	titCell: ".hd ul",
+	mainCell: ".bd ul",
+	effect: "fold",
+	autoPlay: true,
+	autoPage: true,
+	trigger: "click",
+	startFun: function(i) {
+		var curLi = jQuery(".fullSlide .bd li").eq(i);
+		if ( !! curLi.attr("_src")) {
+			curLi.css("background-image", curLi.attr("_src")).removeAttr("_src")
+		}
+	}
+});
+
+jQuery(".crb a").hover(function() {
 
 	$(this).addClass('crbhover');
 
@@ -29,40 +47,6 @@ $('#tracking').blur(function() {
 		$("#tracking").val($(this).attr('fs'));
 	}
 })
-var Restr=/^\d{12}$/;
-
-$("form").submit(function(e) {
-	var sval = $("#tracking").val();
-	sval=sval.replaceAll("(\n|\r|(\r\n)|(\u0085)|(\u2028)|(\u2029))", "");
-	if (sval == $("#tracking").attr('fs') || sval == '') {
-		error(3);
-		return false;
-	}else if(!Restr.test($.trim(sval))){
-		$('#qpart').append('<div id="toast" style="height:35px;display:none;width:246px;_width:260px;*width:260px;position:absolute;left:2px;bottom:48px;background:#fdf6e2;color:#f86d00;font-weight:bold;text-align:center;line-height:35px;font-size:14px;font-weight:bold;">請輸入12位運單號追蹤</div>');
-		$('#toast').stop(true,false).animate({height:"show"},1000,function(){
-			setTimeout(function(){$('#toast').animate({height:'hide'},1000);},1000);
-		});
-		return false;
-	}
-});
-})
-
-jQuery(".fullSlide").slide({
-	titCell: ".hd ul",
-	mainCell: ".bd ul",
-	effect: "fold",
-	autoPlay: true,
-	autoPage: true,
-	trigger: "click",
-	startFun: function(i) {
-		var curLi = jQuery(".fullSlide .bd li").eq(i);
-		if ( !! curLi.attr("_src")) {
-			curLi.css("background-image", curLi.attr("_src")).removeAttr("_src")
-		}
-	}
-});
-
-
 
 
 function normal(times) {
@@ -79,3 +63,22 @@ function error(times) {
 	times = times - 1;
 	setTimeout("normal(" + times + ")", 150);
 }
+var Restr=/^\d{12}$/;
+
+$("form").submit(function(e) {
+	var sval = $("#tracking").val();
+	sval=sval.replaceAll("(\n|\r|(\r\n)|(\u0085)|(\u2028)|(\u2029))", "");
+	if (sval == $("#tracking").attr('fs') || sval == '') {
+		error(3);
+		return false;
+	}else if(!Restr.test($.trim(sval))){
+		$('#qpart').append('<div id="toast" style="height:35px;display:none;width:246px;_width:260px;*width:260px;position:absolute;left:2px;bottom:48px;background:#fdf6e2;color:#f86d00;font-weight:bold;text-align:center;line-height:35px;font-size:14px;font-weight:bold;">請輸入12位運單號追蹤</div>');
+		$('#toast').stop(true,false).animate({height:"show"},1000,function(){
+			setTimeout(function(){$('#toast').animate({height:'hide'},1000);},1000);
+		});
+		return false;
+	}else{
+		window.location.href="http://www.speedoex.com/tc/express?trackid="+sval;
+	}
+	return false;
+});
